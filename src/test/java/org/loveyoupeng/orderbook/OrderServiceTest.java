9a8b4a3802f93d62705105332eb4063bb295c73a
loveyoupeng <loveyoupeng@gmail.com>
2022-03-06 13:06:37 +0000

    timestamp

--- src/test/java/org/loveyoupeng/orderbook/OrderServiceTest.java
+++ src/test/java/org/loveyoupeng/orderbook/OrderServiceTest.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 
+import java.util.concurrent.CountDownLatch;
+
 public class OrderServiceTest {
   private static final Logger logger =
       LoggerFactory.getLogger(OrderServiceTest.class);
@@ -26,7 +28,7 @@ public class OrderServiceTest {
 
   @Test
   public void test_order_service_timmer() throws Exception {
-    when(clock.now()).thenReturn(0L, 100L);
+    when(clock.now()).thenReturn(0L, 100L).thenReturn(150L);
     final long now = clock.now();
     final DeadlineTimerWheel wheel =
         new DeadlineTimerWheel(TimeUnit.MILLISECONDS, now, 1, 2);
@@ -39,12 +41,26 @@ public class OrderServiceTest {
     assertEquals("error scheduled", size,
                  wheel.poll(clock.now(), this::onTimerExpiry, Integer.MAX_VALUE));
 
+    final CountDownLatch latch = new CountDownLatch(1);
     final Disruptor<EventContainer> disruptor =
         new Disruptor<>(EventContainer::new, 16, DaemonThreadFactory.INSTANCE);
-    disruptor.handleEventsWith(this::timestamp);
+    disruptor.handleEventsWith(this::timestamp)
+        .then(this::peek)
+        .then((event, seq, eob) -> {latch.countDown();});
     disruptor.start();
 
+    disruptor.publishEvent(this::emptyEvent);
+
+    latch.await();
+
     disruptor.shutdown();
+  }
+
+  private void emptyEvent(final EventContainer event, final long sequence) {}
+
+  private void peek(final EventContainer event, final long sequence,
+                         final boolean endOfBatch) throws Exception {
+    logger.info("{}", event);
   }
 
   private void timestamp(final EventContainer event, final long sequence,
@@ -80,11 +96,16 @@ final class EventContainer {
     this.event = event;
   }
   
-  public void timestamp(final long timestmap) {
+  public void timestamp(final long timestamp) {
     this.timestamp = timestamp;
   }
 
   public long timestamp() {
     return timestamp;
+  }
+
+  @Override
+  public String toString(){
+    return "event@" + timestamp + " [" + event + "]";
   }
 }
